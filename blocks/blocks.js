6b164a3c36fce8fcef339115609425239e30673b
Max <sonthaya.nt@hotmail.com>
2020-10-08 01:34:52 +0700

    Initial commit

---
+++ blocks/blocks.js
@@ -0,0 +1,58 @@
+Blockly.defineBlocksWithJsonArray([
+{
+  "type": "ir_read",
+  "message0": "IR Remote pin %1 read",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "pin",
+      "options": [
+        [
+          "IN1",
+          "32"
+        ],
+        [
+          "IN2",
+          "33"
+        ],
+        [
+          "IN3",
+          "34"
+        ],
+        [
+          "IN4",
+          "35"
+        ],
+        [
+          "OUT1",
+          "26"
+        ],
+        [
+          "OUT2",
+          "27"
+        ],
+        [
+          "23",
+          "23"
+        ],
+        [
+          "19",
+          "19"
+        ],
+        [
+          "18",
+          "18"
+        ],
+        [
+          "VN",
+          "39"
+        ]
+      ]
+    }
+  ],
+  "output": "Number",
+  "colour": "#515A5A",
+  "tooltip": "Get data from Sony Remote",
+  "helpUrl": ""
+}
+]);
